b9041b0e33b36a3af52cdfa5230c6cea96019155
Andrey Alexeev <alexeev.andrey.a@gmail.com>
2017-12-03 11:12:16 +0300

    4-02

---
+++ _steps_/04/04-02/app/intro/intro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tfs-intro',
+  templateUrl: './intro.component.html',
+  styleUrls: ['./intro.component.css']
+})
+export class IntroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
